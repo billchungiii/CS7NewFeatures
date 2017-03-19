888074268d2e90e49e0b52a7b6668e110070d7cb
billchungiii <billchungiii@gmail.com>
2017-03-19 11:23:09 +0800

    add outsample01

---
+++ OutSample001/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutSample001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s="123";
+           
+            // 以前你要這樣寫
+            int i;
+            int.TryParse(s,out i);
+            Console.WriteLine(i);
+            // 現在你可以直接這樣寫
+            int.TryParse(s, out int j);
+            Console.WriteLine(j);
+
+            // ref 不能比照 out 使用
+            //Test(ref int y = 10);
+
+            Console.ReadLine();
+        }
+
+
+        static void Test(ref int x)
+        {
+            x = x + 1;
+        }
+
+
+    }
+}
--- OutSample001/Properties/AssemblyInfo.cs
+++ OutSample001/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("CS7NewFeatures")]
+[assembly: AssemblyTitle("OutSample001")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CS7NewFeatures")]
+[assembly: AssemblyProduct("OutSample001")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("b3e35055-b6d8-4ba5-af43-0084a9cde4cc")]
+[assembly: Guid("50e3303d-6102-4eb5-834c-0fbaa0b9ae3a")]
 
 // 組件的版本資訊由下列四個值所組成: 
 //
